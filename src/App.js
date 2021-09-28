112b580b84d0c83e180abee948ad14ef3fa5df67
amyeguo <72362221+amyeguo@users.noreply.github.com>
2021-09-28 12:34:31 -0500

    Style appp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 const schedule = {
   title: "CS Courses for 2018-2019",
@@ -34,14 +35,19 @@ const getCourseTerm = (course) => terms[course.id.charAt(0)];
 const getCourseNumber = (course) => course.id.slice(1, 4);
 
 const Course = ({ course }) => (
-  <div>
-    {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">
+        {getCourseTerm(course)} CS {getCourseNumber(course)}
+      </div>
+      <div className="card-text">{course.title}</div>
+    </div>
   </div>
 );
 
 //Course List Component
 const CourseList = ({ courses }) => (
-  <div>
+  <div className="course-list">
     {Object.values(courses).map((course) => (
       <Course key={course.id} course={course} />
     ))}
@@ -53,7 +59,7 @@ const Banner = ({ title }) => <h1>{title}</h1>;
 
 //App
 const App = () => (
-  <div>
+  <div className="container">
     <Banner title={schedule.title} />
     <CourseList courses={schedule.courses} />
   </div>
