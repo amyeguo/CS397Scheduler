36f58a63e9c6813115009b1f7581c21fdf71566f
amyeguo <72362221+amyeguo@users.noreply.github.com>
2021-09-30 00:47:06 -0500

    Filter by time

--- src/App.js
+++ src/App.js
@@ -5,20 +5,97 @@ import "./App.css";
 //List of terms
 const terms = { F: "Fall", W: "Winter", S: "Spring" };
 
+//toggle takes in an element and a list
+//if the element is in the list, then return a list with that element removed
+//otherwise add the element to the list
+const toggle = (x, lst) =>
+  lst.includes(x) ? lst.filter((y) => y !== x) : [x, ...lst];
+
+//select the course term and term number
 const getCourseTerm = (course) => terms[course.id.charAt(0)];
 
 const getCourseNumber = (course) => course.id.slice(1, 4);
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">
-        {getCourseTerm(course)} CS {getCourseNumber(course)}
+//check if courses conflict with each other
+const hasConflict = (course, selected) =>
+  selected.some((selection) => courseConflict(course, selection));
+
+//check the meeting times
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
+
+const mapValues = (fn, obj) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value)])
+  );
+
+const addCourseTimes = (course) => ({
+  ...course,
+  ...timeParts(course.meets),
+});
+
+const addScheduleTimes = (schedule) => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses),
+});
+
+//time conflict
+const days = ["M", "Tu", "W", "Th", "F"];
+
+const daysOverlap = (days1, days2) =>
+  days.some((day) => days1.includes(day) && days2.includes(day));
+
+const hoursOverlap = (hours1, hours2) =>
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end);
+
+const timeConflict = (course1, course2) =>
+  daysOverlap(course1.days, course2.days) &&
+  hoursOverlap(course1.hours, course2.hours);
+
+const courseConflict = (course1, course2) =>
+  getCourseTerm(course1) === getCourseTerm(course2) &&
+  timeConflict(course1, course2);
+
+//course component
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const style = {
+    backgroundColor: isDisabled
+      ? "lightgrey"
+      : isSelected
+      ? "lightgreen"
+      : "white",
+  };
+  return (
+    <div
+      className="card m-1 p-2"
+      style={style}
+      onClick={() => setSelected(toggle(course, selected))}
+    >
+      <div className="card-body">
+        <div className="card-title">
+          {getCourseTerm(course)} CS {getCourseNumber(course)}
+        </div>
+        <div className="card-text">{course.title}</div>
+        <div className="card-text">{course.meets}</div>
       </div>
-      <div className="card-text">{course.title}</div>
     </div>
-  </div>
-);
+  );
+};
 
 const TermButton = ({ term, setTerm, checked }) => (
   <>
@@ -52,6 +129,7 @@ const TermSelector = ({ term, setTerm }) => (
 //Course List Component
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState("Fall");
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(
     (course) => term === getCourseTerm(course)
   );
@@ -61,33 +139,42 @@ const CourseList = ({ courses }) => {
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
         {termCourses.map((course) => (
-          <Course key={course.id} course={course} />
+          <Course
+            key={course.id}
+            course={course}
+            selected={selected}
+            setSelected={setSelected}
+          />
         ))}
       </div>
     </>
   );
 };
 
-//Banner Component (takes in schedule title and destructres)
+//Banner Component (takes in schedule title object and destructres)
 const Banner = ({ title }) => <h1>{title}</h1>;
 
 //App
 const App = () => {
+  //setting the schedule uses state
   const [schedule, setSchedule] = useState();
   const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
 
+  //fetches the schedule asynchronously with the Effect hook
   useEffect(() => {
     const fetchSchedule = async () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     };
     fetchSchedule();
   }, []);
 
+  //while schedule is loading...
   if (!schedule) return <h1>Loading schedule...</h1>;
 
+  //returns a banner with the ti
   return (
     <div className="container">
       <Banner title={schedule.title} />
