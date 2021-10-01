b694349d4095423111071b415bbb9ae32b8bb5bd
amyeguo <72362221+amyeguo@users.noreply.github.com>
2021-09-30 23:37:38 -0500

    Modularize code

---
+++ src/components/Course.js
@@ -0,0 +1,39 @@
+import { hasConflict, getCourseTerm } from "../utilities/times.js";
+
+//toggle takes in an element and a list
+//if the element is in the list, then return a list with that element removed
+//otherwise add the element to the list
+const toggle = (x, lst) =>
+  lst.includes(x) ? lst.filter((y) => y !== x) : [x, ...lst];
+
+const getCourseNumber = (course) => course.id.slice(1, 4);
+
+//course component
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const style = {
+    backgroundColor: isDisabled
+      ? "lightgrey"
+      : isSelected
+      ? "lightgreen"
+      : "white",
+  };
+  return (
+    <div
+      className="card m-1 p-2"
+      style={style}
+      onClick={() => setSelected(toggle(course, selected))}
+    >
+      <div className="card-body">
+        <div className="card-title">
+          {getCourseTerm(course)} CS {getCourseNumber(course)}
+        </div>
+        <div className="card-text">{course.title}</div>
+        <div className="card-text">{course.meets}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Course;
