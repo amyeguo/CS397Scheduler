eb892a665dcc3c655d83545a5080a41ae16aec9d
amyeguo <72362221+amyeguo@users.noreply.github.com>
2021-10-04 01:05:32 -0500

    Add authentication

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,17 +1,49 @@
 import { hasConflict, getCourseTerm } from "../utilities/times.js";
-
+import { setData } from "../utilities/firebase";
 //toggle takes in an element and a list
 //if the element is in the list, then return a list with that element removed
 //otherwise add the element to the list
 const toggle = (x, lst) =>
   lst.includes(x) ? lst.filter((y) => y !== x) : [x, ...lst];
 
+//check the meeting times
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
 const getCourseNumber = (course) => course.id.slice(1, 4);
 
-//course component
+const getCourseMeetingData = (course) => {
+  const meets = prompt("Enter meeting data: MTuWThF hh:mm-hh:mm", course.meets);
+  const valid = !meets || timeParts(meets).days;
+  if (valid) return meets;
+  alert("Invalid meeting data");
+  return null;
+};
+const reschedule = async (course, meets) => {
+  if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+    try {
+      await setData(`/courses/${course.id}/meets`, meets);
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
+
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
-  const isDisabled = !isSelected && hasConflict(course, selected);
+  const isDisabled = hasConflict(course, selected);
   const style = {
     backgroundColor: isDisabled
       ? "lightgrey"
@@ -19,11 +51,13 @@ const Course = ({ course, selected, setSelected }) => {
       ? "lightgreen"
       : "white",
   };
+
   return (
     <div
       className="card m-1 p-2"
       style={style}
-      onClick={() => setSelected(toggle(course, selected))}
+      onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+      onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}
     >
       <div className="card-body">
         <div className="card-title">
@@ -35,5 +69,4 @@ const Course = ({ course, selected, setSelected }) => {
     </div>
   );
 };
-
 export default Course;
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -49,3 +49,5 @@ export const useData = (path, transform) => {
 
   return [data, loading, error];
 };
+
+export const setData = (path, value) => set(ref(database, path), value);
